64e675d9342ff722914810b783c44728764e2882
shepelzz <shepelzz@gmail.com>
2019-08-06 18:03:01 +0300

    post

--- src/main/java/com/api/MainRestController.java
+++ src/main/java/com/api/MainRestController.java
@@ -2,15 +2,27 @@ package com.api;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MainRestController {
 
     @GetMapping(path = "/test")
-    public ResponseEntity<?> test(){
+    public ResponseEntity<?> testGet(){
         return new ResponseEntity<>("tested", HttpStatus.OK);
+    }
+
+    @PostMapping(path = "/test")
+    public ResponseEntity<?> testPost(@RequestBody String text){
+        if(text == null || text.isEmpty())
+            return new ResponseEntity<>("empty value", HttpStatus.BAD_REQUEST);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("Lol: ");
+        for(String s : text.split(""))
+            sb.append(s).append(". ");
+
+        return new ResponseEntity<>(sb.toString(), HttpStatus.OK);
     }
 
 }
