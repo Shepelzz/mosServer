b86b83d7dbc952de2b3f19e7c3eb477d10a7c98b
Zhenya_home <shepelzz@gmail.com>
2019-08-07 23:37:37 +0300

    авп

--- src/main/java/com/api/MainRestController.java
+++ src/main/java/com/api/MainRestController.java
@@ -1,16 +1,19 @@
 package com.api;
 
-import com.models.Account;
 import com.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
+
 @RestController
 public class MainRestController {
     private UserService userService;
+    private static final Logger LOG = LoggerFactory.getLogger(MainRestController.class);
 
     @Autowired
     public MainRestController(UserService userService) {
@@ -22,13 +25,15 @@ public class MainRestController {
         return new ResponseEntity<>("tested", HttpStatus.OK);
     }
 
-    @PostMapping(path = "/login")
+    @PostMapping(path = "/test")
     public ResponseEntity<?> login(@RequestBody String userName){
+        LOG.info("POST - parameter: [{}]", userName);
+
         if(StringUtils.isEmpty(userName))
             return new ResponseEntity<>("Naeb", HttpStatus.BAD_REQUEST);
 
-        Account userAccount = userService.login(userName);
-        return new ResponseEntity<>(userAccount.asLuaTable(), HttpStatus.OK);
+//        Account userAccount = userService.login(userName);
+        return new ResponseEntity<>(userName, HttpStatus.OK);
     }
 
 
