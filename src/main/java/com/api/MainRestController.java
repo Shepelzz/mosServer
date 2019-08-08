0078929a7dcc3a3b8071555e51e6972acec5ad0d
shepelzz <shepelzz@gmail.com>
2019-08-08 14:14:53 +0300

    post

--- src/main/java/com/api/MainRestController.java
+++ src/main/java/com/api/MainRestController.java
@@ -1,10 +1,12 @@
 package com.api;
 
+import com.models.Account;
 import com.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
@@ -20,20 +22,19 @@ public class MainRestController {
         this.userService = userService;
     }
 
-    @GetMapping(path = "/test")
+    @GetMapping(path = "/test", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> testGet(){
         return new ResponseEntity<>("tested", HttpStatus.OK);
     }
 
-    @PostMapping(path = "/test")
-    public ResponseEntity<?> login(@RequestBody String userName){
-        LOG.info("POST - parameter: [{}]", userName);
-
+    @PostMapping(path = "/login")
+    public ResponseEntity<?> login(@ModelAttribute(value = "userName") String userName){
+        LOG.info("POST /login - parameter: [{}]", userName);
         if(StringUtils.isEmpty(userName))
             return new ResponseEntity<>("Naeb", HttpStatus.BAD_REQUEST);
 
-//        Account userAccount = userService.login(userName);
-        return new ResponseEntity<>(userName, HttpStatus.OK);
+        Account userAccount = userService.login(userName);
+        return new ResponseEntity<>(userAccount, HttpStatus.OK);
     }
 
 
